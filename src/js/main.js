7e87aafedd3d81ce7a4d889de28d5c0cf7c3c3f7
Harrison <harrison@theshoebridges.com>
2015-10-19 07:39:12 +1100

    Remove old code

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,3 @@
-/*var ws = new WebSocket('ws://localhost:6060');
-
-ws.onopen = function() {
-    console.log('Opened!');
-    ws.send("SETchat.freenode.net:6667/#roomtest")
-}
-
-ws.onclose = function() {
-    console.log('Closed!');
-}
-
-ws.onerror = function(error) {
-    console.log('We got an error:' + error);
-}
-
-ws.onmessage = function(e) {
-    var m = document.createElement("p");
-    m.textContent = e.data;
-    document.body.appendChild(m);
-}
-*/
-
 var app = angular.module('room', [
     'angular-websocket'
 ]);
